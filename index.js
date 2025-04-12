bdd70b6d37d6f59b9cfc05aa5f6370ca8157d947
Lucas Souza <L.c.s.souzaroots@gmail.com>
2025-04-12 18:47:49 -0300

    event.preventDefault:  método em JavaScript que impede o comportamento padrão de um evento do navegador. É muito usado, por exemplo, para evitar que um formulário seja enviado automaticamente ao clicar no botão submit.

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const inputItem = document.getElementById("input-item")
 const botaoAdicionar = document.getElementById("adicionar-item");
 
 botaoAdicionar.addEventListener("click", (evento) => {
-    console.log(evento)
+    evento.preventDefault();
+    console.log(inputItem.value);
 })
