76f53a096560f4645278661e547aabc8e3efd182
Lucas Souza <L.c.s.souzaroots@gmail.com>
2025-04-17 17:40:51 -0300

    adicionado inputcheckbox id com contador

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inputItem = document.getElementById("input-item")
 
 const botaoAdicionar = document.getElementById("adicionar-item");
+let contador = 0;
 
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault();
@@ -14,6 +15,6 @@ botaoAdicionar.addEventListener("click", (evento) => {
     containerItemDaLista.classList.add("lista-item-container"); // containerItemDaLista junto ao classlist.add para informar qual div estaria pegando a classe definida no html //
     const inputCheckBox = document.createElement("input");
     inputCheckBox.type = "checkbox";
-    
+    inputCheckBox.id = "checkbox-" + contador++;
 
 })
