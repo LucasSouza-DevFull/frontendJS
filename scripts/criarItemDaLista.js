90759db95b4eb561ae77726c6af5a8522e2064f4
Lucas <L.c.s.souzaroots@gmail.com>
2025-07-02 00:13:21 -0300

    Criaçao de arquivo script

---
+++ scripts/criarItemDaLista.js
@@ -0,0 +1,46 @@
+const inputItem = document.getElementById("input-item");
+function criarItemDaLista(){
+evento.preventDefault();
+    if(inputItem.value ==="") {
+        alert("Por favor, insira um item");
+        return
+    }
+
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container"); // containerItemDaLista junto ao classlist.add para informar qual div estaria pegando a classe definida no html //
+    const inputCheckBox = document.createElement("input");
+    inputCheckBox.type = "checkbox";
+    inputCheckBox.id = "checkbox-" + contador++;
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+    inputCheckBox.addEventListener("click", function(){
+        if (inputCheckBox.checked){
+            nomeItem.style.textDecoration = "line-through";
+        } else{
+            nomeItem.style.textDecoration = "none";
+        }
+    })
+
+    containerItemDaLista.appendChild(inputCheckBox);
+    containerItemDaLista.appendChild(nomeItem);
+    
+
+    itemDaLista.appendChild(containerItemDaLista);
+
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
+        weekday: "long",        
+    });
+    const data = new Date().toLocaleDateString("pt-BR");
+    const hora = new Date().toLocaleTimeString("pt-BR", {
+        timeStyle: "short",
+    });
+    const dataFormatada = `${diaDaSemana} (${data}) às ${hora}`;
+    const itemData = document.createElement("p");
+    itemData.classList.add("texto-data");
+    itemData.innerText = dataFormatada;
+    itemDaLista.appendChild(itemData);
+
+    return itemDaLista;
+}
